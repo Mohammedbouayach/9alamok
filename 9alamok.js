e9ff4c3de0efae998fd48f415aa054036b914136
Mohammedbouayach <112516568+Mohammedbouayach@users.noreply.github.com>
2023-07-26 16:11:32 +0100

    Add files via upload

---
+++ 9alamok.js
@@ -0,0 +1,107 @@
+let text = document.getElementById("text");
+
+let name= document.getElementById("name");
+
+let btn= document.getElementById("btn");
+
+let paragraphe = document.getElementById("paragraphe")
+
+
+let jaim = document.getElementById("jaim");
+
+
+
+
+
+
+text.oninput=function(){
+ 
+ if (text.value.trim()!== '') {
+  btn.style.display="inline"
+ }
+ else{
+btn.style.display="none"
+ }
+}
+
+
+
+
+
+
+
+// crat data
+let data;
+if(localStorage.prodact != null){
+ data = JSON.parse(localStorage.prodact)
+}
+else{
+ data= []
+}
+
+
+btn.onclick=function(){
+let newdata = {
+ text:text.value,
+ name:name.value,
+}
+data.push(newdata);
+localStorage.setItem("prodact",JSON.stringify(data));
+
+
+clear()
+read()
+
+
+}
+// clear input
+
+function clear(){
+  text.value="";
+  name.value="";
+  btn.style.display="none"
+}
+
+
+// Read
+
+function read(){
+ let cart = "";
+ for (let i = 0; i < data.length; i++) {
+  cart += `<div class="card">
+  <div class="pen">
+   <p> ${data[i].text}</p>
+   <h3>${ data[i].name} </h3>
+
+  
+   </div>
+   <div class="like">
+   <button  id= "jaim">❤️</button>
+   <p id = "paragraphe"></p>
+   
+   </div>
+ 
+   </div>
+   
+  
+  `
+ 
+ }
+ document.getElementById("card").innerHTML=cart
+}read()
+
+
+// dlet
+
+function delet(i){
+ 
+ data.splice(i,1);
+ localStorage.prodact= JSON.stringify(data);
+ read()
+}
+
+
+
+
+// like
+  /* <button onclick="delet(${i})">مسح</button>*/
